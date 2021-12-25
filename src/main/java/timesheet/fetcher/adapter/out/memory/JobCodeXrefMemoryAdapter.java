fd726d18f24101519176261959a8ce6380633882
Brian Schalme <bschalme@airspeed.ca>
2021-12-25 14:35:03 -0600

    Add rate 2.

--- src/main/java/timesheet/fetcher/adapter/out/memory/JobCodeXrefMemoryAdapter.java
+++ src/main/java/timesheet/fetcher/adapter/out/memory/JobCodeXrefMemoryAdapter.java
@@ -22,12 +22,19 @@ public class JobCodeXrefMemoryAdapter implements JobCodeXrefPort {
                 .qbdJobId("80000162-1618687386")
                 .qbdJobFullName("The Board:Job One")
                 .build());
+        xrefMap.put(60921270, TSheetsJobCodeXref.builder()
+                .tsheetsJobCodeId(60921270)
+                .qbdServiceItemId("80000051-1638646199")
+                .qbdServiceItemFullName("Premium rate 2")
+                .qbdJobId("80000162-1618687386")
+                .qbdJobFullName("The Board:Job One")
+                .build());
         xrefMap.put(49359685, TSheetsJobCodeXref.builder()
                 .tsheetsJobCodeId(49359685)
                 .qbdServiceItemId("ABC-123")
                 .qbdServiceItemFullName("Standard rate")
                 .qbdJobId("DEF-456")
-                .qbdJobFullName("English Harbour:Run Tasting")
+                .qbdJobFullName("English Harbour:Rum Tasting")
                 .build());
         xrefMap.put(58920510, TSheetsJobCodeXref.builder()
                 .tsheetsJobCodeId(58920510)
